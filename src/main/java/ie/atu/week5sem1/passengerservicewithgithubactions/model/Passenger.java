b55a939f7573b464d47d48183757a500314a8955
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-10-15 14:05:34 +0100

    Added variables to passenger

--- src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/model/Passenger.java
+++ src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/model/Passenger.java
@@ -1,4 +1,7 @@
 package ie.atu.week5sem1.passengerservicewithgithubactions.model;
 
 public class Passenger {
+    private String passengerId;
+    private String name;
+    private String email;
 }
