f6589b1ba893dddbf1f18d0a72eb4c789a5d7a50
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-10-15 14:44:07 +0100

    Fixed few naming issues

--- src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/controller/PassengerController.java
+++ src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/controller/PassengerController.java
@@ -2,6 +2,7 @@ package ie.atu.week5sem1.passengerservicewithgithubactions.controller;
 
 import ie.atu.week5sem1.passengerservicewithgithubactions.model.Passenger;
 import ie.atu.week5sem1.passengerservicewithgithubactions.service.PassengerService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,28 +13,28 @@ import java.util.Optional;
 @RequestMapping("/api/passengers")
 public class PassengerController {
 
-     private final PassengerService Service;
+     private final PassengerService service;
 
      @GetMapping
      public PassengerController(PassengerService Service) {
-         this.Service = Service;
+         this.service = Service;
      }
 
      @GetMapping("/{id}")
      public ResponseEntity<Passenger> getOne(@PathVariable String id) {
-         Optional<Passenger> Passenger=Service.findById(id);
+         Optional<Passenger> maybe = service.findById(id);
          if (maybe.isPresent()) {
-             return ResponseEntity.ok(Passenger.get());
+             return ResponseEntity.ok(maybe.get());
          } else {
              return ResponseEntity.notFound().build();
          }
      }
 
      @PostMapping
-     public ResponseEntity<Passenger> create(@RequestBody Passenger Passenger p) {
+     public ResponseEntity<Passenger> create(@Valid @RequestBody Passenger p) {
          Passenger created = service.create(p);
          return ResponseEntity
-                 .create(URI.create("/api/passengers/" + created.getPassengerId()))
+                 .created(URI.create("/api/passengers/" + created.getPassengerId()))
                  .body(created);
      }
 }
