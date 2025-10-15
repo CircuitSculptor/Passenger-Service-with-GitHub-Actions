dc7e1aa1a84f2915b64f81e91696e4bf34d2001a
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-10-15 15:04:35 +0100

    Create test for passenger service

--- src/test/java/ie/atu/week5sem1/passengerservicewithgithubactions/service/PassengerServiceTest.java
+++ src/test/java/ie/atu/week5sem1/passengerservicewithgithubactions/service/PassengerServiceTest.java
@@ -1,4 +1,48 @@
 package ie.atu.week5sem1.passengerservicewithgithubactions.service;
 
+import ie.atu.week5sem1.passengerservicewithgithubactions.model.Passenger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class PassengerServiceTest {
+
+    private PassengerService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new PassengerService();
+    }
+
+    @Test
+    void createThenFindById() {
+        Passenger p = new Passenger("B1", "Bartek", "bartek@atu.ie");
+
+        service.create(p);
+
+        Optional<Passenger> found = service.findById("B1");
+        assertTrue(found.isPresent());
+        assertEquals("Bartek", found.get().getName());
+    }
+
+    @Test
+    void duplicateIdThrows() {
+        service.create(Passenger.builder()
+                .passengerId("B2")
+                .name("Bob")
+                .email("b@atu.ie")
+                .build());
+
+        assertThrows(IllegalArgumentException.class,  () ->
+                service.create(Passenger.builder()
+                        .passengerId("B2")
+                        .name("Bobby")
+                        .email("bob@ex.com")
+                        .build()));
+    }
 }
