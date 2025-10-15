80b648bee7ed958fa7f74d37ddc2e1e77477def5
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-10-15 14:08:18 +0100

    Added lombok and validation

--- src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/model/Passenger.java
+++ src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/model/Passenger.java
@@ -1,7 +1,27 @@
 package ie.atu.week5sem1.passengerservicewithgithubactions.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Passenger {
+    @NotBlank
+    @Size(max = 40)
     private String passengerId;
+
+    @NotBlank
+    @Size(max = 40)
     private String name;
+
+    @NotBlank
+    @Email
     private String email;
 }
