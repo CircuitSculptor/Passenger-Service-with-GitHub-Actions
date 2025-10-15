05d9402cbb09432d189d51f5391f4389ed934c96
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-10-15 15:38:11 +0100

    Test3

--- src/test/java/ie/atu/week5sem1/passengerservicewithgithubactions/service/PassengerServiceTest.java
+++ src/test/java/ie/atu/week5sem1/passengerservicewithgithubactions/service/PassengerServiceTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
+
 public class PassengerServiceTest {
 
     private PassengerService service;
