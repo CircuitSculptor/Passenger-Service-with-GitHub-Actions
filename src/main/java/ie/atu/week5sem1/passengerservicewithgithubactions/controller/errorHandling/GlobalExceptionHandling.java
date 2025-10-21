8758e04319c8a5cea70fbaf56fe5273a2f510c3b
Bartlomiej <g00428194@atu.ie>
2025-10-21 20:51:33 +0100

    Basic Error Handling

---
+++ src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/controller/errorHandling/GlobalExceptionHandling.java
@@ -0,0 +1,14 @@
+package ie.atu.week5sem1.passengerservicewithgithubactions.controller.errorHandling;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandling {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String showErrorDetails()
+    {
+        return "There is an issue";
+    }
+}
