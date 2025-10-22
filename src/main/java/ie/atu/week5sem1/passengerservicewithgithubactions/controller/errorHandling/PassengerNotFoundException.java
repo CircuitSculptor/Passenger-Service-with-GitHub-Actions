120e89f00d6c5cdfba1e5f90f4800339aa36ac6c
Bartlomiej <g00428194@atu.ie>
2025-10-22 12:34:52 +0100

    Created PassengerNotFound Exception

---
+++ src/main/java/ie/atu/week5sem1/passengerservicewithgithubactions/controller/errorHandling/PassengerNotFoundException.java
@@ -0,0 +1,15 @@
+package ie.atu.week5sem1.passengerservicewithgithubactions.controller.errorHandling;
+
+public class PassengerNotFoundException extends RuntimeException
+{
+    private String message;
+    private String field;
+    public PassengerNotFoundException()
+    {
+        this.field = field;
+        this.message = message;
+    }
+    public PassengerNotFoundException(String message) {
+        super(message);
+    }
+}
